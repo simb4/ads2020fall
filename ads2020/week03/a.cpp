8f4bb995a505b23fb96892335f91d5a2e416a221
simb4 <alimzhan.amanov@gmail.com>
2020-09-18 15:01:52 +0600

    week03:II

--- ads2020/week03/a.cpp
+++ ads2020/week03/a.cpp
@@ -6,60 +6,43 @@ using namespace std;
 int a[100], n;
 
 bool bin_search(int *a, int n, int x) {
-	// [0, n-1]
 	int l = 0, r = n - 1;
-	while (l < r) { // l != r
-		int mid = (l + r) / 2;
+	while (l <= r) {
+		int mid = (l + r) / 2; // floor
 		if (a[mid] == x)
 			return true;
-		if (a[mid] < x) { // [    |mid|  x ] 
-			l = mid + 1;
-		} else { // x <= a[mid]:  [ x  |mid|    ] 
+		// [oooooo a[mid] ooxo]
+		if (x < a[mid])
 			r = mid - 1;
-		}
-	}
-	return a[l] == x;
-}
-int left_bin_search(int *a, int n, int x) {
-	/*
-		Find the leftmost occurence of x,
-		if there is one.
-	*/
-	// [0, n-1]
-	int l = 0, r = n - 1;
-	while (l < r) { // l != r
-		int mid = (l + r) / 2;
-		if (a[mid] < x) { // [    |mid|  x ] 
+		else // a[mid] < x
 			l = mid + 1;
-		} else { // x <= a[mid]:  [   x-1|mid|x|x|x+1    ] 
+	}
+	return false;
+}
+
+int left_bin_search(int *a, int n, int x) {
+	int l = 0, r = n - 1;
+	while (l < r) {
+		int mid = (l + r) / 2; // floor
+		if (x <= a[mid])
 			r = mid;
-		}
+		else // a[mid] < x
+			l = mid + 1;
 	}
 	if (a[l] == x)
 		return l;
 	return -1;
 }
+
+// 1 2 3 4 5 6
 int right_bin_search(int *a, int n, int x) {
-	/*
-		Find the rightmost occurence of x,
-		if there is one.
-		Be aware of inf loop, for ex:
-		state of [l, r] = [2, 3]
-		mid = 2
-		a[mid] == x
-		l = 2
-		again state of [l, r] = [2,3]
-		=> inf loop
-	*/
-	// [0, n-1]
 	int l = 0, r = n - 1;
-	while (l < r) { // l != r
-		int mid = (l + r + 1) / 2;
-		if (a[mid] <= x) { // [    |mid|  x ] 
+	while (l < r) {
+		int mid = (l + r + 1) / 2; // ceil
+		if (a[mid] <= x)
 			l = mid;
-		} else { //  x <= a[mid] :  [   x |a[mid]| x x   ] 
+		else // x < a[mid] 
 			r = mid - 1;
-		}
 	}
 	if (a[l] == x)
 		return l;
@@ -80,7 +63,8 @@ int main() {
 		cin >> x;
 		cout << (bin_search(a, n, x) ? "Yes\n" : "No\n");
 		cout << left_bin_search(a, n, x) << " ";
-		cout << right_bin_search(a, n, x) << "\n\n";
+		cout << right_bin_search(a, n, x);
+		cout << "\n\n";
 	}
 
 	return 0;
